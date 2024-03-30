390bc1c17f126b6e6826488ad70dd7a51baa47d2
Jose Gregorio Archila <jarchila@cdainfo.local>
2024-03-30 11:59:17 -0300

    primer ejemplar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import PostAddIcon from '@material-ui/icons/PostAdd';
+import ListIcon from '@material-ui/icons/List';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import './App.css'
+import Home from './pages/Home'
+// import CardList from './card-list/CardList';
+import Products from './pages/Products/Products';
+
+const useStyles = makeStyles((theme) => ({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const AppRoute = () => {
+  console.log("entre");
+  return (
+    <Routes>
+        <Route path="/" element={<Home/>} exact />
+        <Route path="/register" element={<Products/>} exact />
+        {/* Optional index route if no nested routes match */}
+    </Routes>
+  );
+};
 
 function App() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <Router>
+        <div className="App">
+          <List>
+          <ListItem button component="a" href="/">
+              <ListItemText primary="Inicio" />
+            </ListItem>
+            <ListItem button component="a" href="/register">
+              <ListItemText primary="Registro de Equipos" />
+            </ListItem>
+          </List>
+
+        </div>
+      </Router>
+      <Divider />
+    </div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      {['left'].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={toggleDrawer(anchor, true)}>
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Alquiler de productos
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <SwipeableDrawer
+            anchor={anchor}
+            open={state[anchor]}
+            onClose={toggleDrawer(anchor, false)}
+            onOpen={toggleDrawer(anchor, true)}
+          >
+            {list(anchor)}
+          </SwipeableDrawer>
+          
+          <div className="container">
+            <Router><AppRoute />
+            </Router>
+          </div>
+
+        </React.Fragment>
+      ))}
     </div>
   );
 }
-
 export default App;
+
+
+
+
---
+++ src/pages/Home/index.js
@@ -0,0 +1,125 @@
+import React, {useState, useEffect} from 'react'
+import './styles.css';
+import data from "./TemplateData.json";
+import { Container, Card, Row, Col, Button } from 'react-bootstrap';
+import './Card.css'
+
+
+const Home = () => {
+  //setear los hooks useState
+  const [ products, setProducts ] = useState([])
+  const [ search, setSearch ] = useState("")
+
+  //función para traer los datos de la API
+  const url = "http://localhost:8080/api/product/getAllProducts";
+
+  const showData = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    //console.log(data)
+    setProducts(data)
+  }   
+   //función de búsqueda
+  const searcher = (e) => {
+      setSearch(e.target.value)   
+  }
+   //metodo de filtrado 1 
+   /*  let results = []
+   if(!search)
+   {
+       results = users
+   }else{
+        results = users.filter( (dato) =>
+        dato.name.toLowerCase().includes(search.toLocaleLowerCase())
+    )
+   } */
+
+   //metodo de filtrado 2   
+   const results = !search ? products : products.filter((dato)=> dato.item.toLowerCase().includes(search.toLocaleLowerCase()))
+  
+   useEffect( ()=> {
+    showData()
+  }, [])
+  
+  //renderizamos la vista
+  return (
+    <Container>
+    <h2> Busqueda de Productos</h2>
+        <input value={search} onChange={searcher} type="text" placeholder='Search' className='form-control'/>
+        <br />
+                { results.map( (product) => (
+                   <Card
+                   key={product.id}
+                   style={{ width: "25%" }}                                    >
+                   <div className='card' key={product.id}>
+                       <div className="card_logo">
+                           <img src={product.url} alt="" />
+                       </div>
+                       <div className="card_data">
+                           <h2>{product.item}</h2> 
+                           <br></br> 
+                           <p><h4> {product.use}</h4></p>
+                           <p>{product.price}$ | {product.date_reserva} </p>
+                           <p>{product.condition} </p>
+                           
+                       </div>
+                       
+                   </div>      
+               </Card>                
+                ))}
+    </Container>
+  )
+}
+export default Home
+
+// function Home() {
+//     const [searchTerm, setSearchTerm] = useState("");
+//     return (
+//         <>
+//             <div className="templateContainer">
+//                 <div className="searchInput_Container">
+//                     <input id="searchInput" type="text" placeholder="Busqueda de equipos..." onChange={(event) => {
+//                         setSearchTerm(event.target.value);
+//                     }} />
+//                 </div>
+//                 <div className="template_Container">
+//                     {
+//                         data.filter((data) => {
+//                             if (searchTerm == "") {
+//                                 return data;
+//                             } else if (data.producto.toLowerCase().includes(searchTerm.toLowerCase())) {
+//                                 return data;
+//                             }
+//                         })
+//                             .map((data, idx) => {
+//                                 return (
+//                                     <Card
+//                                         key={idx}
+//                                         style={{ width: "25%" }}                                    >
+//                                         <div className='card card-personalizada' key={data.id}>
+//                                             <div className="card_logo">
+//                                                 <img src={data.url} alt="" />
+//                                             </div>
+//                                             <div className="card_data">
+//                                                 <h2>{data.producto}</h2> 
+//                                                 <br></br> 
+//                                                 <p><h4> {data.uso}</h4></p>
+//                                                 <p>{data.price}$ | {data.date_reserva} </p>
+//                                                 <p>{data.condicion} </p>
+                                                
+//                                             </div>
+                                            
+//                                         </div>      
+//                                     </Card>
+//                                 )
+//                             })
+//                     }
+//                 </div>
+                
+//             </div>
+
+//         </>
+//     )
+// }
+
+// export default Home
